36c345bfbafafc13f7abb4cba5c51ad882d24f09
nikkzzzyu <nikzzzlumaguemerjoy@gmail.com>
2024-02-15 10:08:46 +0800

    Added a date in Main

--- script/main.js
+++ script/main.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const title = document.getElementById('title');
     const welcomeText = document.getElementById('welcome');
     const enterButton = document.querySelector('.enter-button');
+    const additionalText = document.querySelector('#additional-text');
     
     // Fade in animation for "Welcome to"
     welcomeText.classList.add('fadeIn');
@@ -14,6 +15,7 @@ document.addEventListener('DOMContentLoaded', function () {
     // Show the enter button after the typewriter effect is complete
     setTimeout(function () {
         enterButton.style.opacity = '1'; // Set opacity to 1 to trigger the CSS transition
+        additionalText.style.opacity = '1'; // Set opacity to 1 for fade-in effect
     }, 3000 + ("Lantern Rite!".length * 200)); // Adjust the delay based on the typewriter speed
     
     function typeWriterEffect(text, element, speed) {
